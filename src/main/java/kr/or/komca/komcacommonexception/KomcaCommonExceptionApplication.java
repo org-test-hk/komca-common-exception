605d0f296fb4f84c53b3cf57f480e688ffb8ae48
kun <sus5330@icloud.com>
2025-01-20 11:04:40 +0900

    first commit

---
+++ src/main/java/kr/or/komca/komcacommonexception/KomcaCommonExceptionApplication.java
@@ -0,0 +1,13 @@
+package kr.or.komca.komcacommonexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomcaCommonExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomcaCommonExceptionApplication.class, args);
+	}
+
+}
