2a5c112cb2f0f8556cf6a421ffce6e1f3db6bce3
kun <sus5330@icloud.com>
2025-02-03 14:47:10 +0900

    temp

--- src/main/java/kr/or/komca/komcacommonexception/config/ExceptionAutoConfiguration.java
+++ src/main/java/kr/or/komca/komcacommonexception/config/ExceptionAutoConfiguration.java
@@ -3,15 +3,21 @@ package kr.or.komca.komcacommonexception.config;
 import kr.or.komca.komcacommonexception.handler.BaseGlobalExceptionHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
+@ConditionalOnProperty(
+        prefix = "komca.exception",
+        name = "enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class ExceptionAutoConfiguration {
+
     @Bean
-    @ConditionalOnMissingBean
-    public BaseGlobalExceptionHandler globalExceptionHandler() {
+    public BaseGlobalExceptionHandler BaseGlobalExceptionHandler() {
         return new BaseGlobalExceptionHandler();
     }
 }
